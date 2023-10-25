f2b27dd51fcad5f4176e4afdac812eb9b25de046
Vahagn Gevorgyan <gevorgyanvahag45@gmail.com>
2023-10-25 23:36:02 +0400

    commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import Header from "../Header/Header";
+import Header from "../Header";
+
+import NewsCard from "../NewsCard";
 
 
-export default function App(){
-
-
-    return(
-      <Header/>  
-    )
+interface News {
+  id: number;
+  title: string;
+  image: string;
+  content: string;
+  pubDate: string;
 }
+
+const App: React.FC = () => {
+
+
+  const news= {
+    id: 1,
+    title: '1 News Title',
+    image: "fffff",
+    content: 'Lorem ipsum dolor, sit amet',
+    pubDate: '17.03.2022',
+  }
+
+  return (
+    <div>
+    <Header/>
+    <NewsCard news={news}/>
+    </div>
+  )
+}
+
+export default App
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import styles from './header.module.css'
+import styles from './Header.module.css'
 
-export default function Header(){
+  
+const Header: React.FC = () => {
 
-    return(
-      <div className={styles.menu}>
-        <div> <a href="#"><span>Logo</span></a></div>
-        <div className={styles.list}> 
-            <ul>
-                <li>Home</li>
-                <li>Contact</li>
-                <li>About us</li>
-            </ul>
-        </div>
-      </div>  
+
+    return (
+        <header className={styles.menu}>
+            <div> <a href="#"><span>Logo</span></a></div>
+            <nav className={styles.list}>
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About us</li>
+                </ul>
+            </nav>
+        </header>
     )
 }
+
+export default Header
